0baa6bea7f25697f0c6d181047d09c678d8afaf4
Ajay Mondal <mrakmondal6612@gmail.com>
2024-11-28 23:34:29 +0530

    zod

--- Backend/Day-02/index.js
+++ Backend/Day-02/index.js
@@ -1,56 +1,53 @@
 const express = require("express");
 const app = express();
+const zod = require("zod");
 const PORT = 8000;
+
+const UserSchema = zod.string().email();
+const passSchema = zod.string().min(6);
 
 const AuthMiddleware = (req, res, next) => {
     const username = req.headers.username;
     const password = req.headers.password;
-    if (username != "ajay@gmail.com" || password != "123") {
-        return res.status(403).json({
-            msg: "invalid user"
-        });
+    const userRes = UserSchema.safeParse(username);
+    const pasRes = passSchema.safeParse(password)
+    if(!(userRes.success) || !(pasRes.success) ){
+        return res.status(403).json({msg:"user invalid"})
     }
+
+    // if (username != "ajay@gmail.com" || password != "123") {
+    //     return res.status(403).json({
+    //         msg: "invalid user"
+    //     });
+    // }
     next();
 
 };
 
-// app.use(AuthMiddleware);
-
-app.get("/", (req, res) => {
-    res.send("<h2>Home!</h2>");
-});
-
-app.get("/health-checkup",AuthMiddleware, (req, res) => {
-
-    // kidney-checkup...
+const KidneyMiddleware = (req, res, next) => {
     const Kidneys = req.query.kidney;
     if (Kidneys != 1 && Kidneys != 2) {
         return res.status(401).json({ msg: "wromg input ! " });
     }
+    next();
+}
 
-    res.send("<h3>Your Health is Fine ! </h3>");
-});
+app.use(AuthMiddleware);
 
-
-
-app.get("/user",AuthMiddleware,(req, res) => {
+app.get("/user", (req, res) => {
     res.send("<h2>Wellecome Back Sir !</h2>");
 });
 
-app.get("/kidney-checkup", (req, res) => {
+// app.get("/kidney-checkup", KidneyMiddleware, (req, res) => {
 
-    // kidney-checkup...
-    const Kidneys = req.query.kidney;
-    if (Kidneys == 1 || Kidneys == 2) {
-        res.send("<h3>Your Kidneys is Fine ! </h3>");
-    } else {
-        res.status(401).json({ msg: "wromg input ! " });
-    }
+//     res.send("<h3>Your Kidneys is Fine ! </h3>");
 
-});
+// });
 
+// app.get("/health-checkup", KidneyMiddleware, (req, res) => {
 
-
+//     res.send("<h3>Your Health is Fine ! </h3>");
+// });
 
 
 app.listen(PORT, (req, res) => {
