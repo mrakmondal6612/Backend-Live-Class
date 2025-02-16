981a3b9b280f23f130f440f0a90bf871d0307655
Ajay Mondal <121559193+mrakmondal6612@users.noreply.github.com>
2025-02-16 18:04:23 +0000

    class in done

---
+++ Backend/Day-00/02-Class/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("This Root Route");
+});
+app.get("/conver", (req, res) => {
+  res.send("This Conver Route");
+});
+app.get("/home", (req, res) => {
+  res.send("This Home Route");
+});
+
+app.listen(8080, (req, res) => {
+  console.log("Server is running on PORT 8080");
+});
