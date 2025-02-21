ef5e195715beb4bfbbb26eb82aebd8aa134c10be
Ajay Mondal <121559193+mrakmondal6612@users.noreply.github.com>
2025-02-21 09:35:39 +0000

    Added Students Folders

---
+++ Students/Aritra/Day-00/01-class/Assignments/index.js
@@ -0,0 +1,4 @@
+// Create a HTTP Server with some end-points.
+// create Home Route (/) to show a outputs "This is Home Route".
+// Create Route, thats gives a inputs and return output (response) to the clint
+//    -> Name & Age (as a input) || Outputs : Hi, I am Ajay Mondal, and I am 18 year's old.
