69ee4d11b95122955e9958d1d1508fcac35f6d5b
ANKAN BERA <beraankan8@gmail.com>
2025-02-24 22:45:11 +0530

    Update index.js

--- Students/Ankan/Day-00/01-class/Assignments/index.js
+++ Students/Ankan/Day-00/01-class/Assignments/index.js
@@ -2,3 +2,23 @@
 // create Home Route (/) to show a outputs "This is Home Route".
 // Create Route, thats gives a inputs and return output (response) to the clint
 //    -> Name & Age (as a input) || Outputs : Hi, I am Ajay Mondal, and I am 18 year's old.
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("This is Home Route");
+});
+
+app.post("/hii", (req, res) => {
+  res.send("Hello from ankan");
+});
+
+app.get("/greet", (req, res) => {
+  const { name, age } = req.query;
+  res.send(`Hi, I amm ${name},and I am ${age} years old`);
+});
+
+app.listen(8080, () => {
+  console.log("server is running on port 8080...");
+});
