65b4f8c46d6d3b2624dd60dc2099644b199b163d
AK0fficial <mr.aktv2529@gmail.com>
2025-02-26 05:36:29 +0000

    ok

--- Backend/Day-00/02-Class/index.js
+++ Backend/Day-00/02-Class/index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is Home Route");
+  res.send("This is Root Route");
 });
 app.get("/conver", (req, res) => {
   res.send("This Conver Route");
 });
+app.get("/name", (req, res) => {
+  res.send("Hello Ajay");
+});
 app.get("/home", (req, res) => {
   res.send("This Home Route");
 });
+
 
 app.listen(8080, (req, res) => {
   console.log("Server is running on PORT 8080...");
